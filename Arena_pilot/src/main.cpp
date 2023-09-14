88d7883cd8fc9e3d5f8425b29e371ebc43a06ccc
Szymon Nowacki <szymon.nowacki@flytronic.pl>
2023-09-14 09:20:37 +0200

    Added comments

--- Arena_pilot/src/main.cpp
+++ Arena_pilot/src/main.cpp
@@ -8,42 +8,50 @@
 #define CH1_OFF_PIN   15
 #define TEST_PIN      34
 
+//adres radiowy stacji do któ©ej ma być wysyłane
 #define RELAY_STATION_ADRESS 1
 
+
+//program wykożystuje deep sleep do oszczędzania energii. Zasypia zawsze na koniec pętli setup,
+//a po obudzeniu przez przycisk zaczyna cały program od początku (od setup znowu).
+//dlatego nie ma pętli loop.
+
 void setup() {
+
+  //inicjalizacja obiektów
+
   Serial.begin(9600);
 
   Oled::init();
-
-  //pinMode(23, INPUT_PULLDOWN);
 
   if(Lora::Radio::init())
         Serial.println("LoRa Started");
     else
         Serial.println("LoRa ERROR");
+
   Lora::Encryption::init();
+
   Lora::start_radio();
-  //esp_sleep_enable_ext0_wakeup((gpio_num_t) 12,1);
-  //esp_sleep_enable_ext0_wakeup((gpio_num_t) 13,1);
+
   esp_sleep_enable_ext1_wakeup(0x40000F000,ESP_EXT1_WAKEUP_ANY_HIGH);
 
 
-}
+  //obsługa przycisków po obudzeniu
 
-void loop() {
-  Serial.println("im up/");
-  //Serial.println("Going to sleep");
   delay(100);
-  
-  //esp_light_sleep_start();
 
   uint64_t GPIO_reason = esp_sleep_get_ext1_wakeup_status();
   uint16_t pressed_pin = (log(GPIO_reason))/log(2);
   Serial.print("GPIO that triggered the wake up: GPIO ");
   Serial.println(pressed_pin, 0);
 
+  // [0,0], [0,1] - on, off przekaźnika 0
+  // [1,0], [1,1] - on, off przekaźnika 1
+  // [0] - test (chodzi o to żeby wiadomość miała 1 znak)
+
   switch(pressed_pin)
   {
+    //kanał 0
     case CH0_ON_PIN:
       {byte msg[2] = {0,1};
       Serial.println(Lora::send_with_ack(RELAY_STATION_ADRESS,msg,2));}
@@ -52,6 +60,8 @@ void loop() {
       {byte msg[2] = {0,0};
       Serial.println(Lora::send_with_ack(RELAY_STATION_ADRESS,msg,2));}
     break;
+
+    //kanał 1
     case CH1_ON_PIN:
       {byte msg[2] = {1,1};
       Serial.println(Lora::send_with_ack(RELAY_STATION_ADRESS,msg,2));}
@@ -60,13 +70,17 @@ void loop() {
       {byte msg[2] = {1,0};
       Serial.println(Lora::send_with_ack(RELAY_STATION_ADRESS,msg,2));}
     break;
+
+    //test
     case TEST_PIN:
       {byte msg[1] = {0};
       if(Lora::send_with_ack(RELAY_STATION_ADRESS,msg,1) > 0)
       {
+        Oled::drawBigText(0,0,F("Test OK"));
         Serial.println("Test: ok");
       }else
       {
+        Oled::drawBigText(0,0,F("Test FAILED"));
         Serial.println("Test: failed");
       }
       
@@ -76,6 +90,14 @@ void loop() {
   }
 
   delay(1000);
+
+  Oled::clear();
+  Oled::sleepDisplay();
   
+
   esp_deep_sleep_start();
+}
+
+void loop() {
+  //nic się nie dzieje
 }
--- Arena_relay/src/main.cpp
+++ Arena_relay/src/main.cpp
@@ -2,9 +2,11 @@
 #include "../lib/oled.h"
 #include "../lib/lora/loraTransport.h"
 
+//piny przekaźników
 #define RELAY_0 0
 #define RELAY_1 2
 
+//adres radiowy pilota, od którego ma odbierać komendy
 #define REMOTE_ADRESS 0
 
 void setup() {
@@ -19,10 +21,10 @@ void setup() {
         Serial.println("LoRa Started");
     else
         Serial.println("LoRa ERROR");
+
   Lora::Encryption::init();
+
   Lora::start_radio();
-
-
 }
 
 void loop() {
@@ -35,9 +37,9 @@ void loop() {
   int received = Lora::try_receive(&addr,msg,&msg_size);
 
   //sprawdzenie czy coś przyszło. status RECEIVED oznacza nową wiadomość
+  //interesują nas tylko wiadomości od pilota
   if(received == Lora::RECEIVED && addr == REMOTE_ADRESS)
   {
-      //można teraz na przykład wypisać otrzymaną wiadomość
       Serial.print("Received packet: ");
       for(int i=0;i<msg_size;i++)
       {
@@ -45,20 +47,22 @@ void loop() {
           Serial.print(" ");
       }
 
+      //wiadomość o długości 2 oznacza polecenie przełączenia
       if(msg_size > 1)
       {
-        if(msg[0] == 0)
+        if(msg[0] == 0) //przekaźnik 0
         {
           if(msg[1] == 0)
-            digitalWrite(RELAY_0, LOW);
-          else
             digitalWrite(RELAY_0, HIGH);
-        }else
+          else
+            digitalWrite(RELAY_0, LOW);
+        }
+        else //przekaźnik 1
         {
           if(msg[1] == 0)
-            digitalWrite(RELAY_1, LOW);
-          else
             digitalWrite(RELAY_1, HIGH);
+          else
+            digitalWrite(RELAY_1, LOW);
         }
       }
   }
